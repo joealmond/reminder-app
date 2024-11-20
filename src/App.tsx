6dd5af4d2f7edf5915e51b0051acf5640121baf6
joealmond <jozsef.mandula@gmail.com>
2024-11-21 00:00:04 +0100

    added feature delete reminder

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,13 @@ function App() {
     setReminders(reminders)
   }
 
+  const removeReminder = (id: number): void => {
+    setReminders(reminders.filter(reminder => reminder.id !== id))
+  }
+
   return (
     <div className="App">
-      <ReminderList reminderItems={reminders}/>
+      <ReminderList reminderItems={reminders} onRemoveReminder={removeReminder}/>
     </div>
   );
 }
--- src/components/ReminderList.tsx
+++ src/components/ReminderList.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Reminder from '../models/reminder';
+import { on } from 'events';
 
 interface ReminderListProps {
     reminderItems: Reminder[];
+    onRemoveReminder: (id: number) => void;
 }
 
-function ReminderList({ reminderItems }: ReminderListProps) {
+function ReminderList({ reminderItems, onRemoveReminder }: ReminderListProps) {
     return (
         <div>
             <ul className='list-group'>
-                {reminderItems.map((item)=> <li className='list-group-item' key={item.id}>{item.title}</li>)}
+                {reminderItems.map((item)=> <li className='list-group-item' key={item.id}>{item.title}
+                    <button onClick={() => onRemoveReminder(item.id)} className='btn mx-2 btn-outline-danger rounded-pill'>Delete</button>
+                </li>)}
             </ul>
         </div>
     );
